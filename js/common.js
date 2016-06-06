b768fe3f91e7fff6cece65235168d470673c87c7
alex <sania_free@i.ua>
2016-06-06 18:24:33 +0300

    index

--- js/common.js
+++ js/common.js
@@ -10,4 +10,17 @@ $(document).ready(function() {
 	
 	//bxslider
 	$('.top_slider ul').bxSlider();
+	$('.rev_slider ul').bxSlider();
+
+
+	//scroll
+	 $('.totop a').on('click',function(){
+		$('html,body').animate({scrollTop:$($(this).attr('href')).offset().top-55},800);
+		return false;
+	});
+
+	 //menu
+	 $( ".menu_trigger" ).click(function() {
+	  $( ".menu ul" ).slideToggle( "slow");
+	});
 });
